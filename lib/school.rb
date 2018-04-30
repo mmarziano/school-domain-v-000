2f367617deac3848e2cbcbfd76ac26f62ca59fec
Monica Marziano <mmarziano1@gmail.com>
2018-04-30 12:04:04 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+
+class School
+    attr_accessor :roster, :name
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+  
+   def add_student(student_name, grade)
+     @roster[grade] << student_name
+   end
+    
+  
+end 
